198ac8ff7ca9919888bd90a67a9974578795453c
ondi <thy321@gmail.com>
2020-05-08 20:39:19 +0300

    typo fix

--- sign.go
+++ sign.go
@@ -15,12 +15,12 @@ type Sign_t struct {
 	sign jwt.Signer
 }
 
-func NewSign(self *Sign_t, AuthKey string) (res Sign_t, err error) {
-	res.sign = &jwt.Sign_t{}
+func NewSign(AuthKey string) (res Sign_t, err error) {
 	var buf []byte
 	if buf, err = ioutil.ReadFile(AuthKey); err != nil {
 		return
 	}
+	res.sign = &jwt.Sign_t{}
 	err = res.sign.LoadKeyPem(buf)
 	return
 }
