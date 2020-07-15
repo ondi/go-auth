b859ceb0fccd077ec3d719c7580a9d7831888966
ondi <thy321@gmail.com>
2020-07-15 18:54:04 +0300

    middleware1 removed

--- middleware.go
+++ middleware.go
@@ -14,16 +14,17 @@ import (
 )
 
 type Middleware_t struct {
-	verify Verify_t
-	except *tst.Tree1_t
-	next   http.Handler
+	verify     Verify_t
+	except     *tst.Tree1_t
+	next_ok    http.Handler
+	next_error http.Handler
 }
 
-func (self Middleware_t) WithNext(next http.Handler) http.Handler {
-	return Middleware_t{verify: self.verify, except: self.except, next: next}
+func (self Middleware_t) WithNext(next_ok http.Handler, next_error http.Handler) http.Handler {
+	return Middleware_t{verify: self.verify, except: self.except, next_ok: next_ok, next_error: next_error}
 }
 
-func NewMiddleware(AuthGlob string, except map[string]string, next http.Handler) (res Middleware_t, err error) {
+func New(AuthGlob string, except map[string]string, next_ok http.Handler, next_error http.Handler) (res Middleware_t, err error) {
 	if res.verify, err = NewVerify(AuthGlob); err != nil {
 		return
 	}
@@ -35,7 +36,8 @@ func NewMiddleware(AuthGlob string, except map[string]string, next http.Handler)
 		}
 		res.except.Add(k, re)
 	}
-	res.next = next
+	res.next_ok = next_ok
+	res.next_error = next_error
 	return
 }
 
@@ -46,18 +48,18 @@ func (self Middleware_t) Names() []string {
 func (self Middleware_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if re, ok := self.except.Search(r.URL.Path).(*regexp.Regexp); ok {
 		if addr := RemoteAddr(r); re.MatchString(addr) {
-			self.next.ServeHTTP(w, r)
+			self.next_ok.ServeHTTP(w, r)
 		} else {
-			http.Error(w, "ACCESS DENIED: "+addr, http.StatusForbidden)
+			self.next_error.ServeHTTP(w, r)
 		}
 		return
 	}
 	ts := time.Now().Unix()
 	payload, ok, err := self.verify.Check(r.Header["Authorization"], ts, ts)
 	if ok && err == nil {
-		self.next.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), auth_key_t("AUTH"), payload)))
+		self.next_ok.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), auth_key_t("AUTH"), payload)))
 	} else {
-		http.Error(w, "Authorization required", http.StatusUnauthorized)
+		self.next_error.ServeHTTP(w, r)
 	}
 	return
 }
--- misc.go
+++ misc.go
@@ -10,7 +10,15 @@ import (
 	"net/http"
 )
 
+var E401 = E401_t{}
+
 type auth_key_t string
+
+type E401_t struct{}
+
+func (E401_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "AUTHORIZATION REQUIRED "+RemoteAddr(r), http.StatusUnauthorized)
+}
 
 func Auth(ctx context.Context) (res map[string]interface{}, ok bool) {
 	res, ok = ctx.Value(auth_key_t("AUTH")).(map[string]interface{})
