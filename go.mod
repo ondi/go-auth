2c256423f8b402694fae7abfdbc4888db6545717
ondi <thy321@gmail.com>
2022-02-01 12:53:07 +0300

    minor fix

--- go.mod
+++ go.mod
@@ -3,6 +3,6 @@ module github.com/ondi/go-auth
 go 1.17
 
 require (
-	github.com/ondi/go-jwt v0.0.0-20211123091646-067fedf5e181
+	github.com/ondi/go-jwt v0.0.0-20220201094853-f97e1a555e1a
 	github.com/ondi/go-tst v0.0.0-20210920150321-3f1dfea3e805
 )
--- sign.go
+++ sign.go
@@ -6,24 +6,40 @@ package auth
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
+	"strings"
 
 	"github.com/ondi/go-jwt"
 )
 
-type Sign_t struct {
-	sign jwt.Signer
+type Signer interface {
+	Sign(bits int64, payload []byte, out *bytes.Buffer) error
 }
 
-func NewSign(AuthKey string) (res Sign_t, err error) {
+type Sign_t struct {
+	jwt.Signer
+}
+
+func NewSign(file string) (res Sign_t, err error) {
 	var buf []byte
-	if buf, err = ioutil.ReadFile(AuthKey); err != nil {
+	if buf, err = ioutil.ReadFile(file); err != nil {
 		return
 	}
-	res.sign, err = jwt.NewSignPem(buf)
+	if strings.HasSuffix(file, ".der") {
+		res.Signer, err = jwt.NewSignDer(buf)
+	} else {
+		res.Signer, err = jwt.NewSignPem(buf)
+	}
 	return
 }
 
-func (self Sign_t) Sign(bits int64, payload []byte) (bytes.Buffer, error) {
-	return jwt.Sign(self.sign, bits, payload)
+func (self Sign_t) Sign(bits int64, payload []byte, out *bytes.Buffer) error {
+	return jwt.Sign(self.Signer, bits, payload, out)
+}
+
+type NoSing_t struct{}
+
+func (NoSing_t) Sign(bits int64, payload []byte, out *bytes.Buffer) error {
+	return fmt.Errorf("NOT INITIALIZED")
 }
