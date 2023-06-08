947d32b3c20cb37a02e4f8232a465d4f2096acfd
0nd1 <thy321@gmail.com>
2023-06-09 00:28:15 +0300

    minor fix

--- middleware.go
+++ middleware.go
@@ -36,66 +36,21 @@ func (self ValidatorList) Validate(ts time.Time, token_name string, in map[strin
 }
 
 type TokenAddr_t struct {
-	token      Token_t
-	verify     Verifier
-	validate   ValidatorList
-	addr       Addr_t
-	except     *tst.Tree1_t[*regexp.Regexp]
-	next_ok    http.Handler
-	next_error http.Handler
+	to *TokenOnly_t
+	ao *AddrOnly_t
 }
 
-func NewTokenAddr(verify Verifier, except map[string]string, next_ok http.Handler, next_error http.Handler, addr Addr_t, token Token_t, validate ...Validator) (self *TokenAddr_t, err error) {
-	self = &TokenAddr_t{
-		token:      token,
-		verify:     verify,
-		validate:   validate,
-		addr:       addr,
-		except:     &tst.Tree1_t[*regexp.Regexp]{},
-		next_ok:    next_ok,
-		next_error: next_error,
+func NewTokenAddr(next_ok http.Handler, next_error http.Handler, token Token_t, addr Addr_t, except map[string]string, verify Verifier, validate ...Validator) (self *TokenAddr_t, err error) {
+	self = &TokenAddr_t{}
+	if self.ao, err = NewAddrOnly(next_ok, next_error, addr, except); err != nil {
+		return
 	}
-
-	var re *regexp.Regexp
-	for k, v := range except {
-		if re, err = regexp.Compile(v); err != nil {
-			return
-		}
-		self.except.Add(k, re)
-	}
-
+	self.to, err = NewTokenOnly(next_ok, self.ao, token, verify, validate...)
 	return
 }
 
 func (self *TokenAddr_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	var count int
-	ts := time.Now()
-	ctx := r.Context()
-	for _, token := range self.token(r) {
-		if payload, ok := self.verify.Verify([]byte(token.Value)); ok {
-			var values map[string]interface{}
-			if json.Unmarshal(payload, &values) != nil {
-				continue
-			}
-			if !self.validate.Validate(ts, token.Name, values) {
-				continue
-			}
-			ctx = WithValue(ctx, token.Name, values)
-			count++
-		}
-	}
-	if count > 0 {
-		self.next_ok.ServeHTTP(w, r.WithContext(ctx))
-		return
-	}
-	re, ok := self.except.Search(r.URL.Path)
-	if ok {
-		if addr := self.addr(r); re.MatchString(addr) {
-			self.next_ok.ServeHTTP(w, r)
-			return
-		}
-	}
-	self.next_error.ServeHTTP(w, r)
+	self.to.ServeHttp(w, r)
 }
 
 type TokenOnly_t struct {
@@ -106,14 +61,15 @@ type TokenOnly_t struct {
 	next_error http.Handler
 }
 
-func NewTokenOnly(verify Verifier, next_ok http.Handler, next_error http.Handler, token Token_t, validate ...Validator) *TokenOnly_t {
-	return &TokenOnly_t{
+func NewTokenOnly(next_ok http.Handler, next_error http.Handler, token Token_t, verify Verifier, validate ...Validator) (self *TokenOnly_t, err error) {
+	self = &TokenOnly_t{
 		token:      token,
 		verify:     verify,
 		validate:   validate,
 		next_ok:    next_ok,
 		next_error: next_error,
 	}
+	return
 }
 
 func (self *TokenOnly_t) ServeHttp(w http.ResponseWriter, r *http.Request) {
@@ -141,26 +97,38 @@ func (self *TokenOnly_t) ServeHttp(w http.ResponseWriter, r *http.Request) {
 }
 
 type AddrOnly_t struct {
-	re         *regexp.Regexp
 	addr       Addr_t
+	except     *tst.Tree1_t[*regexp.Regexp]
 	next_ok    http.Handler
 	next_error http.Handler
 }
 
-func NewAddrOnly(re *regexp.Regexp, next_ok http.Handler, next_error http.Handler, addr Addr_t) *AddrOnly_t {
-	return &AddrOnly_t{
-		re:         re,
+func NewAddrOnly(next_ok http.Handler, next_error http.Handler, addr Addr_t, except map[string]string) (self *AddrOnly_t, err error) {
+	self = &AddrOnly_t{
 		addr:       addr,
 		next_ok:    next_ok,
 		next_error: next_error,
 	}
+
+	var re *regexp.Regexp
+	for k, v := range except {
+		if re, err = regexp.Compile(v); err != nil {
+			return
+		}
+		self.except.Add(k, re)
+	}
+
+	return
 }
 
 func (self *AddrOnly_t) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	if addr := self.addr(r); self.re.MatchString(addr) {
-		self.next_ok.ServeHTTP(w, r)
-	} else {
-		self.next_error.ServeHTTP(w, r)
+	re, ok := self.except.Search(r.URL.Path)
+	if ok {
+		if addr := self.addr(r); re.MatchString(addr) {
+			self.next_ok.ServeHTTP(w, r)
+			return
+		}
 	}
+	self.next_error.ServeHTTP(w, r)
 	return
 }
