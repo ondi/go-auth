842417c7eb377e9a3f506e2240d7e278ba67bc9a
ondi <thy321@gmail.com>
2024-04-02 23:40:25 +0300

    minor fix

--- middleware_test.go
+++ middleware_test.go
@@ -11,10 +11,10 @@ import (
 )
 
 func Test001(t *testing.T) {
-	parse_bearer, err := NewParseBearer(1, nil)
+	parse_bearer, err := NewParseBearer(nil, nil)
 	assert.NilError(t, err)
 
-	parse_basic, err := NewParseBasic(1, nil)
+	parse_basic, err := NewParseBasic(nil)
 	assert.NilError(t, err)
 
 	bearer := NewAuth(nil, nil, parse_bearer, NewFindBearer(NewTokenBearer(NewExp(-60, 60))))
