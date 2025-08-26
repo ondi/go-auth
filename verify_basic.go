09471d8cfe58c3172ce2cecc9f6b8f86a0525e9a
ondi <thy321@gmail.com>
2025-08-26 19:06:44 +0300

    minor fix

--- verify_basic.go
+++ verify_basic.go
@@ -5,28 +5,26 @@
 package auth
 
 import (
-	"bytes"
-	"encoding/base64"
 	"net/http"
 	"regexp"
 )
 
 type VerifyBasic_t struct {
 	find    TokenFinder
-	verify  map[string]struct{}
+	verify  map[string]string
 	approve *regexp.Regexp
 }
 
 func NewVerifyBasic(keys []string, approve string, find TokenFinder) (self *VerifyBasic_t, err error) {
 	self = &VerifyBasic_t{
 		find:   find,
-		verify: map[string]struct{}{},
-	}
-	for _, v := range keys {
-		self.verify[v] = struct{}{}
+		verify: map[string]string{},
 	}
 	if self.approve, err = regexp.Compile(approve); err != nil {
 		return
+	}
+	for _, v := range keys {
+		self.verify[v] = ""
 	}
 	return
 }
@@ -36,16 +34,9 @@ func (self *VerifyBasic_t) TokenFind(r *http.Request) (keys_found int, out []Tok
 }
 
 func (self *VerifyBasic_t) Verify(token []byte) (payload []byte, key_id string, err error) {
-	payload = make([]byte, base64.URLEncoding.DecodedLen(len(token)))
-	n, err := base64.URLEncoding.Decode(payload, token)
-	if err != nil {
-		return
-	}
-	payload = payload[:n]
-	if _, ok := self.verify[string(token)]; ok {
-		if ix := bytes.IndexByte(payload, ':'); ix > -1 {
-			key_id = string(payload[:ix])
-		}
+	var ok bool
+	if key_id, ok = self.verify[string(token)]; ok {
+		payload = token
 		return
 	}
 	err = ERROR_VERIFY_FAILED
